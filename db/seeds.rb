120cf3da46930c0fcecae4a66ab22116a09631da
Asami Takaoka <asamiyamabe61@gmail.com>
2025-03-08 14:45:08 +0900

    conflictの解消

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 特権管理者を作成（存在しない場合のみ）
+AdminUser.find_or_create_by!(username: 'admin') do |admin|
+  admin.password = 'UMtDj4ZBv%&d@Tzh'
+  admin.password_confirmation = 'UMtDj4ZBv%&d@Tzh'
+  admin.role = 1  # roleカラムで特権管理者かどうかを区別する
+end
