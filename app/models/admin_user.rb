5118d4755a2b5825788388a93141b27a8121910f
Asami Takaoka <asamiyamabe61@gmail.com>
2025-03-08 13:57:51 +0900

    特権管理者のseedデータ生成

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,12 @@
 class AdminUser < ApplicationRecord
   has_secure_password  # bcrypt を使ったパスワード認証を有効化
 
-  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
   validates :role, presence: true, inclusion: { in: [ 0, 1 ] } # 0: 一般管理者, 1: 特権管理者
+
+  # 特権管理者を判定するメソッド
+  def superadmin?
+    role == 1
+  end
 end
