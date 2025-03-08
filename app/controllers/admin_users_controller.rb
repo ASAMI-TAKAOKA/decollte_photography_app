db8fa68956661262ede7c222fa30e131bd7650b3
Asami Takaoka <asamiyamabe61@gmail.com>
2025-03-08 15:42:53 +0900

    admin_users_controllerとadmin_userモデルを修正

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,70 +1,49 @@
 class AdminUsersController < ApplicationController
-  before_action :set_admin_user, only: %i[ show edit update destroy ]
+  # 管理者でないとアクセスできないアクションに制限をかける
+  before_action :check_admin, only: [:create, :update, :destroy]
 
-  # GET /admin_users or /admin_users.json
   def index
     @admin_users = AdminUser.all
   end
 
-  # GET /admin_users/1 or /admin_users/1.json
   def show
+    @admin_user = AdminUser.find(params[:id])
   end
 
-  # GET /admin_users/new
-  def new
-    @admin_user = AdminUser.new
-  end
-
-  # GET /admin_users/1/edit
-  def edit
-  end
-
-  # POST /admin_users or /admin_users.json
   def create
     @admin_user = AdminUser.new(admin_user_params)
-
-    respond_to do |format|
-      if @admin_user.save
-        format.html { redirect_to @admin_user, notice: "Admin user was successfully created." }
-        format.json { render :show, status: :created, location: @admin_user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @admin_user.errors, status: :unprocessable_entity }
-      end
+    if @admin_user.save
+      redirect_to @admin_user, notice: 'Admin user was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /admin_users/1 or /admin_users/1.json
   def update
-    respond_to do |format|
-      if @admin_user.update(admin_user_params)
-        format.html { redirect_to @admin_user, notice: "Admin user was successfully updated." }
-        format.json { render :show, status: :ok, location: @admin_user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @admin_user.errors, status: :unprocessable_entity }
-      end
+    @admin_user = AdminUser.find(params[:id])
+    if @admin_user.update(admin_user_params)
+      redirect_to @admin_user, notice: 'Admin user was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /admin_users/1 or /admin_users/1.json
   def destroy
-    @admin_user.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to admin_users_path, status: :see_other, notice: "Admin user was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @admin_user = AdminUser.find(params[:id])
+    @admin_user.destroy
+    redirect_to admin_users_url, notice: 'Admin user was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_user
-      @admin_user = AdminUser.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def admin_user_params
-      params.expect(admin_user: [ :username, :password_digest, :role ])
+  # 管理者かどうかを確認
+  def check_admin
+    unless current_user.role == 1  # roleが1でないユーザーは管理者ではない
+      redirect_to root_path, alert: 'You do not have permission to perform this action.'  # アクセス拒否
     end
+  end
+
+  def admin_user_params
+    params.require(:admin_user).permit(:username, :password, :role)
+  end
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,7 @@
 class AdminUser < ApplicationRecord
   has_secure_password  # bcrypt を使ったパスワード認証を有効化
 
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
   validates :role, presence: true, inclusion: { in: [ 0, 1 ] } # 0: 一般管理者, 1: 特権管理者
 end
