039d161e9f3c34a4177c4023992a46743717b5da
Asami Takaoka <asamiyamabe61@gmail.com>
2025-03-20 16:10:19 +0900

    ブランドに紐づく店舗一覧と紐づかない店舗一覧でそれぞれ並び替えを可能に(routeの設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,28 @@
 Rails.application.routes.draw do
   root to: "brands#index"
 
-  # GET: ユーザーがログイン画面を開くときに使用
+  # 管理者ログイン関連
   get "admin/login", to: "admin_users#login", as: "admin_users_login"
-  # POST: ログイン処理の実行時に使用
   post "admin/login", to: "admin_users#login"
   delete "logout", to: "admin_users#logout", as: "admin_users_logout"
   resources :admin_users
   get "admin/dashboard", to: "admin_users#dashboard", as: "admin_dashboard"
+
+  # ブランドと店舗関連
   resources :brands, param: :slug do
     resources :stores, param: :id do
       member do
-        get :move_higher
-        get :move_lower
+        get :move_higher_within_brand
+        get :move_lower_within_brand
       end
+    end
+  end
+
+  # 全店舗一覧の並び替え
+  resources :stores, only: %i[index] do
+    member do
+      get :move_higher
+      get :move_lower
     end
   end
 end
