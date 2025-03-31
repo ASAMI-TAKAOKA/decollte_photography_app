f9e216ba5ea4605d2333e690c4dd33d117770049
Asami Takaoka <asamiyamabe61@gmail.com>
2025-04-01 07:59:11 +0900

    並び替え機能: 全店舗一覧での機能を削除, acts_as_listを使用したブランドごとの店舗一覧での機能のみとする

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,22 +1,20 @@
 class Store < ApplicationRecord
   belongs_to :brand, optional: true
 
-  # ブランドごとの店舗一覧での順番を管理する
-  acts_as_list scope: :brand, column: :position
-
-  # 全店舗一覧での順番を管理する
-  # before_create により、店舗作成(create)の直前に実行される
-  before_create :set_global_position
+  acts_as_list scope: :brand
 
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
 
-  private
-
-  # Store.maximum(:global_position).to_i + 1 で、 現在の最大global_positionに1を足して設定
-  # 新しく作られた店舗は 一番最後に追加されることになる
-  def set_global_position
-    self.global_position = Store.maximum(:global_position).to_i + 1
+  def move_within_brand(direction)
+    case direction
+    when :higher
+      move_higher
+    when :lower
+      move_lower
+    else
+      raise ArgumentError, "Invalid direction: #{direction}"
+    end
   end
 end
