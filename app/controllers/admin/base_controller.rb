186b650423805a634aa9eaf9cbb78ce957b6813a
Asami Takaoka <asamiyamabe61@gmail.com>
2025-04-10 00:35:44 +0900

    helper_method :current_user, :admin_logged_in?は全共通コントローラに移す

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,22 +1,11 @@
 module Admin
   class BaseController < ApplicationController
     before_action :check_login
-    helper_method :current_user, :admin_logged_in?
 
     private
 
     def check_login
       redirect_to new_admin_session_path, alert: "ログインが必要です。" unless session[:admin_user_id]
-    end
-
-    def current_user
-      return nil unless session[:admin_user_id].present?
-
-      @admin_user ||= AdminUser.find(session[:admin_user_id])
-    end
-
-    def admin_logged_in?
-      session[:admin_user_id].present?
     end
 
     def prohibit_access_for_regular_admin
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_user, :admin_logged_in?
 
+  def current_user
+    return nil unless session[:admin_user_id].present?
+
+    @admin_user ||= AdminUser.find(session[:admin_user_id])
+  end
+
+  def admin_logged_in?
+    session[:admin_user_id].present?
+  end
 end
